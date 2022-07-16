676de5938c2180872dc3ffaf7059f87a916fcc1d
JamesGardiner1 <jg20145@essex.ac.uk>
2022-07-16 20:56:39 +0100

    removed headless argument for webdriver

--- cogs/ai_images/dalle_cog.py
+++ cogs/ai_images/dalle_cog.py
@@ -102,7 +102,7 @@ class dalle(commands.Cog):
         #standard options work fine but window pops up when command runs
         options = webdriver.ChromeOptions()
         options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        options.add_argument("--headless")
+        #options.add_argument("--headless")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
 
