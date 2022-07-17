1fa0ec38808efccd5d8c6bcba69c34ebbb83f869
JamesGardiner1 <jg20145@essex.ac.uk>
2022-07-17 22:19:58 +0100

    removed all BrianBotConfig imports

--- cogs/test.py
+++ cogs/test.py
@@ -2,7 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import aiohttp
-import BrianBotConfig as config
 
 class test(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -13,4 +12,4 @@ class test(commands.Cog):
         await interaction.response.send_message(f"My name is: {name} and my age is: {age}")
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(test(bot), guilds=[discord.Object(id=config.TEEF2_SERVER)])
+    await bot.add_cog(test(bot))
