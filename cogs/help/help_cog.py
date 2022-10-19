e4506159d1a1157efde9585ca559485972e4a44f
JamesGardiner1 <jg20145@essex.ac.uk>
2022-10-19 22:39:39 +0100

    changed help command to be group cog

--- cogs/help/help_cog.py
+++ cogs/help/help_cog.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -8,17 +9,17 @@ import random
 import os
 from config import GLOBAL_SYNC
 
-class Help(commands.Cog):
+class Brian(commands.GroupCog, name="brian"):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"Help cog is now ready. Synced Globally: {GLOBAL_SYNC}")
+        print(f"Brian cog is now ready. Synced Globally: {GLOBAL_SYNC}")
 
     @app_commands.command(name="help", description="Get help with Brians commands")
     async def help_command(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Bot Brian Help", description="Description", color=discord.Color.from_rgb(0, 255, 0))
+        embed = discord.Embed(title="Bot Brian Help", description="", color=discord.Color.from_rgb(0, 255, 0))
         for c in self.bot.cogs:
             cog = self.bot.get_cog(c)
             group = cog.app_command
@@ -31,7 +32,7 @@ class Help(commands.Cog):
 async def setup(bot: commands.Bot) -> None:
     if GLOBAL_SYNC:
         # Global Sync
-        await bot.add_cog(Help(bot))
+        await bot.add_cog(Brian(bot))
     else:
         # Private Sync
-        await bot.add_cog(Help(bot), guilds=[discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"])])
+        await bot.add_cog(Brian(bot), guilds=[discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"])])
