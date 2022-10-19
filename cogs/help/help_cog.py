3146093c5855fd14349895be6c868072fcce07df
JamesGardiner1 <jg20145@essex.ac.uk>
2022-10-19 22:29:00 +0100

    added basic help command

--- cogs/help/help_cog.py
+++ cogs/help/help_cog.py
@@ -1,91 +1,37 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from typing import Optional, Set
+import re
+import requests
+from bs4 import BeautifulSoup
+import random
 import os
 from config import GLOBAL_SYNC
 
-class MyHelpCommand(commands.MinimalHelpCommand):
-    def get_command_signature(self, command):
-        return f"{self.clean_prefix}{command.qualified_name} {command.signature}"
-
-    async def help_embed(self, title: str, description: Optional[str] = None, mapping: Optional[dict] = None,
-        command_set: Optional[Set[commands.Command]] = None):
-        embed = discord.Embed(title=title)
-        if description:
-            embed.description = description
-        if command_set:
-            filtered = await self.filter_commands(command_set, sort=True)
-            for command in filtered:
-                embed.add_field(name=self.get_command_signature(command), value=command.short_doc or "...",
-                                inline=False)
-        if mapping:
-            for cog, command_set in mapping.items():
-                filtered = await self.filter_commands(command_set, sort=True)
-                if not filtered:
-                    continue
-                name = cog.qualified_name if cog else "No category"
-
-                cmd_list = "\u2002".join(f"`{self.clean_prefix}{cmd.name}`" for cmd in filtered)
-                value = (f"{cog.description}\n{cmd_list}"
-                        if cog and cog.description
-                        else cmd_list
-                        )
-                embed.add_field(name=name, value=value, inline=False)
-
-        return embed
-
-    async def send_bot_help(self, mapping: dict):
-        embed = await self.help_embed(title="Bot Commands",
-                                      description=self.context.bot.description,
-                                      mapping=mapping)
-
-        embed.add_field(name="__Command Info__",
-                        value=f"`<...>` _Required argument_\n`[...]` _Optional argument_",
-                        inline=False)
-
-        await self.get_destination().send(embed=embed)
-
-    async def send_command_help(self, command: commands.Command):
-        embed = await self.help_embed(
-            title=f"={command.qualified_name}",
-            description=command.help,
-            command_set=command.commands if isinstance(command, commands.Group) else None
-        )
-
-        await self.get_destination().send(embed=embed)
-
-    async def send_cog_help(self, cog: commands.Cog):
-        embed = await self.help_embed(
-            title=cog.qualified_name,
-            description=cog.description,
-            command_set=cog.get_commands()
-        )
-
-        await self.get_destination().send(embed=embed)
-
-    send_group_help = send_command_help
-
-class help(commands.Cog, name="Help"):
-    """Shows help info about commands"""
-
+class Help(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self._original_help_command = bot.help_command
-        bot.help_command = MyHelpCommand()
-        bot.help_command.cog = self
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"MyHelpCommand cog is now ready. Synced Globally: {GLOBAL_SYNC}")
+        print(f"Help cog is now ready. Synced Globally: {GLOBAL_SYNC}")
 
-    def cog_unload(self):
-        self.bot.help_command = self._original_help_command
+    @app_commands.command(name="help", description="Get help with Brians commands")
+    async def help_command(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Bot Brian Help", description="Description", color=discord.Color.from_rgb(0, 255, 0))
+        for c in self.bot.cogs:
+            cog = self.bot.get_cog(c)
+            group = cog.app_command
+            embed.add_field(name=group.name.upper(), value=f"".join(f"`{command.name}`: {command.description}\n" for command in group.commands), inline=False)
+        
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+            
+
 
 async def setup(bot: commands.Bot) -> None:
     if GLOBAL_SYNC:
         # Global Sync
-        await bot.add_cog(MyHelpCommand(bot))
+        await bot.add_cog(Help(bot))
     else:
         # Private Sync
-        await bot.add_cog(MyHelpCommand(bot), guilds=[discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"])])
+        await bot.add_cog(Help(bot), guilds=[discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"])])
--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class MyBot(commands.Bot):
         
     async def setup_hook(self):
         self.session = aiohttp.ClientSession()
-        await self.load_extension(f"cogs.test")
         await self.load_extension(f"cogs.words.dictionaries_cog")
         await self.load_extension(f"cogs.music.music_cog")
         await self.load_extension(f"cogs.nsfw.r34_cog")
@@ -27,47 +26,6 @@ class MyBot(commands.Bot):
         print(f"{self.user} has conected to Discord! Synced Globally: {GLOBAL_SYNC}")
 
 bot = MyBot()
-
-@bot.command(name="globalcmdsync")
-async def global_sync_command(ctx: commands.Context):
-    if not ctx.author.id == 153945414683328513:
-        return await ctx.reply("You do not have permission to use this command.")
-    guild = ctx.guild
-    await bot.tree.copy_global_to(guild=guild)
-    print("All commands synced globally")
-
-
-@bot.command(name="privatecmdsync")
-async def private_sync_command(ctx: commands.Context):
-    if not ctx.author.id == 153945414683328513:
-        return await ctx.reply("You do not have permission to use this command.")
-    await bot.tree.sync(guild=discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"]))
-    print("Private commands synced")
-
-@bot.command(name="removeallcmds")
-async def remove_all_commands(ctx: commands.Context):
-    if not ctx.author.id == 153945414683328513:
-        return await ctx.reply("You do not have permission to use this command.")
-    bot.tree.sync(guild=discord.Object(id=os.environ["DEVELOPMENT_SERVER_ID"]))
-    print("Synced")
-    bot.tree.remove_command("join")
-    bot.tree.remove_command("leave")
-    bot.tree.remove_command("play")
-    bot.tree.remove_command("stop")
-    bot.tree.remove_command("pause")
-    bot.tree.remove_command("resume")
-    bot.tree.remove_command("search")
-    bot.tree.remove_command("seek")
-    bot.tree.remove_command("loop")
-    bot.tree.remove_command("queue")
-    bot.tree.remove_command("skip")
-    bot.tree.remove_command("introduce")
-    bot.tree.remove_command("define")
-    bot.tree.remove_command("urban_define")
-    bot.tree.remove_command("r34")
-    bot.tree.remove_command("dalle")
-    bot.tree.remove_command("deepai")
-    print("All commands removed.")
 
 bot.run(os.environ["DISCORD_TOKEN"])
 
